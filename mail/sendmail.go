6b21f5f4391545641b3110d1e54f239ed6e1819a
WPFS <823431828@qq.com>
2021-01-04 14:32:39 +0800

    增加config.json配置文件

--- mail/sendmail.go
+++ mail/sendmail.go
@@ -1,7 +1,10 @@
 package mail
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"errors"
+	"net"
 	"net/smtp"
 	"os"
 	"strings"
@@ -14,6 +17,68 @@ type cfg struct {
 	Password string
 	To       string
 	Subject  string
+}
+
+func Dial(addr string) (*smtp.Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
+}
+
+func validateLine(line string) error {
+	if strings.ContainsAny(line, "\n\r") {
+		return errors.New("smtp: A line must not contain CR or LF")
+	}
+	return nil
+}
+
+func SendMailSSL(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	for _, recp := range to {
+		if err := validateLine(recp); err != nil {
+			return err
+		}
+	}
+	c, err := Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	if a != nil {
+		if ok, _ := c.Extension("AUTH"); !ok {
+			return errors.New("smtp: server doesn't support AUTH")
+		}
+		if err = c.Auth(a); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
 
 func Sendmail(body string) {
@@ -38,7 +103,7 @@ func Sendmail(body string) {
 		"\r\n" +
 		body + "\r\n")
 	toUser := strings.Split(conf.To, ";")
-	err = smtp.SendMail(conf.MailHost+":"+conf.MailPort, auth, conf.From, toUser, msg)
+	err = SendMailSSL(conf.MailHost+":"+conf.MailPort, auth, conf.From, toUser, msg)
 	if err != nil {
 		panic(err)
 	}
